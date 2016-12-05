c8fcd7fe136c5b21545347a29b08d451c40e593e
Robin Ricard <ricard.robin@gmail.com>
2016-12-05 22:55:11 +0100

    Reproduce a failing CRA case

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+/* @flow */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+var stylemistake: number = 'this is wrong, this var isn not even used';
